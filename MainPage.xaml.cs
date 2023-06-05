c20370ab29d84ef000c1183ec72ed1d77cd1f77d
Douglas-Thornton <douglas.thornton@chamonix.com.au>
2023-06-05 11:18:11 +0930

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MAUIVetAPP1;
+
+public partial class MainPage : TabbedPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+
+        // Clear the SecureStorage when the application is closed
+        SecureStorage.Remove("currentCount");
+    }
+}
---
+++ Pages/NativeButtonTest.xaml.cs
@@ -0,0 +1,9 @@
+namespace MAUIVetAPP1.Pages;
+
+public partial class NativeButtonTest : ContentPage
+{
+	public NativeButtonTest()
+	{
+		InitializeComponent();
+	}
+}
